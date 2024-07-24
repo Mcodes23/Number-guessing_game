66d5e8fdf758d5784a6e356b57292ddb0483380a
Mcodes23 <mateliboniface@gmail.com>
2024-07-24 06:57:22 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+let flag = true;
+while (flag) {
+  let randomNumber = Math.floor(Math.random() * 10) + 1;
+  let userNumber = +prompt("Enter a number between 1 and 10: ");
+
+  if (
+    userNumber == null ||
+    isNaN(userNumber) ||
+    userNumber < 1 ||
+    userNumber > 100
+  ) {
+    alert("Invalid input Enter a number between 1 and 10");
+  } else if (userNumber < randomNumber) {
+    alert("too low try again");
+  } else if (userNumber > randomNumber) {
+    alert("too high try again");
+  } else {
+    alert("You win!");
+    flag = false;
+  }
+}
